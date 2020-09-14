654a41a738f10dbf996e5310e97cde0ac5dba09b
aaandrades <aaandrades@yahoo.com.co>
2020-09-14 11:01:37 -0500

    Proyecto completo de Angular, consumo Local, no Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//Rutas
+import { APP_ROUTING } from "./app.routes";
+
+//Servicios
+import { HeroesService } from './components/services/heroes.service';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { HeroesComponent } from './components/heroes/heroes/heroes.component';
+import { AboutComponent } from './components/about/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeroesComponent,
+    AboutComponent,
+    HeroeComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTING,
+    
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
